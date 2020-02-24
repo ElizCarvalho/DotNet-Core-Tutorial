433e02a63bd90eb24d4effd1d4f23485606b523e
Eliz Carvalho <elizabeth.carvalho@radixeng.com.br>
2020-02-24 18:56:52 -0300

    Aula de Rotas

--- Projeto_Web_1/Controllers/AdminController.cs
+++ Projeto_Web_1/Controllers/AdminController.cs
@@ -2,12 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Projeto_Web_1.Controllers
 {
+    [Route("Painel/Admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index(){
-            return Content("Olá, eu sou o Olaf!");
+
+        [HttpGet("{nome}/{numero:int?}")] // ':int' estou tipando o parametro e o '?' torna opcional
+        [HttpGet("Principal/{nome}/{numero:int?}")]
+        public IActionResult Index(string nome, int numero = 99){
+            return Content("Olá, eu sou o Olaf! Eu tenho " + numero + " anos. Tudo bem " + nome + " ?");
         }
 
+        [HttpGet("Olafinho")]
         public IActionResult Olaf(){
             return Content("Eu gosto de abraços quentinhos :)");
         }
